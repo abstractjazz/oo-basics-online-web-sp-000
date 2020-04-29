63a11a361daca0d7ea2e6bbcb06e7c251ca5302c
Evan Burton <evan@heyevan.com>
2020-04-29 05:08:31 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,22 +1,11 @@
 require 'pry'
 
 class Shoe
-attr_accessor :brand, :color, :size, :material, :condition
-
+attr_accessor :brand, :color, :size, :material,
 def initialize(brand)
   @brand = brand
   end
 
 def cobble
 puts "Your shoe is as good as new!"
-condition = "new" 
   end
-
-def condition=(condition)
-  @shoe_condition = condition
-end
-
-def condition
-@shoe_condition
-  end
-end
